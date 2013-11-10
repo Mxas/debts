a07fb8e93723a5f99a74ba731dada8cd6d596887
Mindaugas <spamas1985@gmail.com>
2013-11-10 13:42:19 +0200

    seleniu tests starts

---
+++ src/test/java/eu/fourFinance/web/selenium/HappyPathWebSeleniuTest.java
@@ -0,0 +1,62 @@
+package eu.fourFinance.web.selenium;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
+import eu.fourFinance.services.EvaluationService;
+import eu.fourFinance.testsuites.categories.UnitTests;
+
+@Category(UnitTests.class)
+public class HappyPathWebSeleniuTest {
+
+	private static final String DEFAULT_WAIT_PERIOD = "3000";
+	private Selenium selenium;
+	private WebDriver driver = new FirefoxDriver();
+
+	@Before
+	public void setUp() throws Exception {
+		String host = "http://localhost:8080/";
+		String browser = "*firefox";
+		// String browser = "*iexplore";
+
+		// configure the selenium client
+		selenium = new DefaultSelenium("localhost", 4444, browser, host);
+
+		// launch the browser window
+		selenium.start();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+
+	@Test
+	public void verifySimpleFormAndRespone() {
+		selenium.open("loan/");
+		selenium.waitForPageToLoad(DEFAULT_WAIT_PERIOD);
+
+		String rate = selenium.getValue("rate");
+
+		// String name = "Patricia";
+		// String surname = "Persson";
+
+		// selenium.type("name", name);
+		// selenium.type("surname", surname);
+
+		// selenium.click("submit");
+		// selenium.waitForPageToLoad(DEFAULT_WAIT_PERIOD);
+
+		assertEquals(Integer.parseInt(rate), EvaluationService.LOAN_RATE);
+		// assertTrue(selenium.isTextPresent(surname));
+	}
+
+}
---
+++ src/test/java/eu/fourFinance/web/selenium/LoginPageWebtest.java
@@ -0,0 +1,37 @@
+package eu.fourFinance.web.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+ 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+ 
+/**
+ * Simple web test that just queries the login page through the controller.
+ * @author Florian Hopf, Synyx GmbH & Co. KG, hopf@synyx.de
+ */
+public class LoginPageWebtest {
+ 
+    @Test
+    public void testPage() {
+        WebDriver driver = new HtmlUnitDriver();
+ 
+        driver.get("http://localhost:8081/url/that/redirects/to/login/");
+ 
+        try {
+            // Find the text input element by its name
+            WebElement element = driver.findElement(By.name("username"));
+ 
+            assertNotNull(element);
+        } catch (NoSuchElementException ex) {
+            fail("Startup of context failed. See console output for more information, : " + ex.getMessage());
+        }
+ 
+        //Close the browser
+        driver.quit();
+    }
+}
